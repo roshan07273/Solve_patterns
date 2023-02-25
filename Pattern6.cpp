21c8bd02de1820c249dd09366058a905c7da69f4
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-02-25 21:06:03 +0530

    Update Pattern6.cpp

--- Pattern6.cpp
+++ Pattern6.cpp
@@ -1,4 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void print2(int n) {
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++)
+            {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+int main(){
+        int t;
+        cin >> t;
+        for(int i=0; i<t;i++){
+        int n;
+        cin >> n;
+        print2(n);
+        }
 Your Output: 
 1 2 3 4 5
 1 2 3 4
