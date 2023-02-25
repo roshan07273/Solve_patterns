01df94983880edff5709e2d9dc4384212fcaa14d
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-02-25 20:52:28 +0530

    Update Pattern4.cpp

--- Pattern4.cpp
+++ Pattern4.cpp
@@ -1,4 +1,8 @@
- for(int i=1;i<=n;i++){
+#include<bits/stdc++.h>
+using namespace std;
+void print2(int n) {
+        
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++)
             {
                 cout << i << " ";
@@ -6,3 +10,18 @@
             cout << endl;
         }
     }
+int main(){
+        int t;
+        cin >> t;
+        for(int i=0; i<t;i++){
+        int n;
+        cin >> n;
+        print2(n);
+        }
+}
+Output: 
+1
+2 2
+3 3 3
+4 4 4 4 
+5 5 5 5 5
