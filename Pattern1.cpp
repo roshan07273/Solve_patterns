ccf5a3731f3c617efdb30b85c69b868beb9c1f80
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-02-25 16:55:52 +0530

    Update Pattern1.cpp

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -3,8 +3,9 @@
 * * * * *
 * * * * *
 * * * * *
-
-void printSquare(int n) {
+#include<bits/stdc++.h>
+using manespace std;
+void print1(int n) {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++)
@@ -14,3 +15,8 @@ void printSquare(int n) {
             cout << endl;
         }
     }
+int main(){
+        int n;
+        cin >> n;
+        print1(n);
+}
