d55b338fd26b410d8f7622e2c8a0cf02e100b342
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-02-26 08:16:13 +0530

    Update Pattern4.cpp

--- Pattern4.cpp
+++ Pattern4.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void print2(int n) {
+void print4(int n) {
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++)
@@ -16,7 +16,7 @@ int main(){
         for(int i=0; i<t;i++){
         int n;
         cin >> n;
-        print2(n);
+        print4(n);
         }
 }
 Output: 
